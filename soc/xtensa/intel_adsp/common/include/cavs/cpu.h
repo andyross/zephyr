e514c51e002625b57e4802a4f537f8a2a1a80b6f
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2020-10-08 15:31:18 +0200

    intel-adsp: disable SMP support in sync with SOF
    soc/xtensa/intel_adsp/common/include/cavs/cpu.h redefines
    PLATFORM_CORE_COUNT and PLATFORM_MASTER_CORE_ID which are already
    defined in SOF. Be careful to avoid conflicts. Ideally one of the
    two files should be removed.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- soc/xtensa/intel_adsp/common/include/cavs/cpu.h
+++ soc/xtensa/intel_adsp/common/include/cavs/cpu.h
@@ -13,10 +13,16 @@
 #ifndef __CAVS_CPU_H__
 #define __CAVS_CPU_H__
 
+#ifndef PLATFORM_CORE_COUNT
 /** \brief Number of available DSP cores (conf. by kconfig) */
+#ifndef CONFIG_SOF
 #define PLATFORM_CORE_COUNT	CONFIG_CORE_COUNT
+#else
+#define PLATFORM_CORE_COUNT	1
+#endif
 
 /** \brief Id of master DSP core */
 #define PLATFORM_MASTER_CORE_ID	0
+#endif
 
 #endif /* __CAVS_CPU_H__ */
