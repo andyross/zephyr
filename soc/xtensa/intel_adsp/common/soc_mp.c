1782a893ba1d0e91b193c40c0a0b0949ffaa5bc4
Andy Ross <andrew.j.ross@intel.com>
2021-08-28 12:10:14 -0600

    soc: intel_adsp: Fix IDC masking & state issues
    Fix various bugs with the new IDC layer that show up in edge cases
    where code relies on correct timing of IPIs (unsurprisingly there is a
    lot of code that recovers anyway even if the IPI doesn't arrive
    promptly).  Leaving this as a separate patch because the prior code in
    the PR has already been reviewed and it "mostly" worked:
    
    The unmasking of the L2 interrupt bit (remember there are three layers
    of masking of the IDC interrupt) was always operating on CPU0 at CPU
    startup because the code had been copied blindly.  Unmask the CPU
    we're actually launching.  It turns out cAVS 2.x re-masks this on CPU
    launch automatically.
    
    The global init code to unmask all these interrupts at startup had the
    same bug, even though it turned out to be needless (the initialization
    state has it unmasked until it turns it back off).  Do it right
    anyway.  Similarly add code to clear out existing interrupt latch
    state by ACKing all IDC interrupts at startup.  Seems needless, but
    behavior isn't documented so let's be safe.
    
    Flag CPU0 as always "active" for the purposes of IPIs.  Forgot to do
    this earlier, oops.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/common/soc_mp.c
+++ soc/xtensa/intel_adsp/common/soc_mp.c
@@ -425,7 +425,7 @@ void idc_isr(void *param)
 	 * CPU".
 	 */
 	for (int i = 0; i < CONFIG_MP_NUM_CPUS; i++) {
-		IDC[start_rec.cpu].core[i].tfc = BIT(31);
+		IDC[prid()].core[i].tfc = BIT(31);
 	}
 }
 
@@ -457,9 +457,18 @@ void soc_idc_init(void)
 		/* Also unmask the IDC interrupt for every core in the
 		 * L2 mask register.
 		 */
-		CAVS_INTCTRL->l2.clear = CAVS_L2_IDC;
+		CAVS_INTCTRL[core].l2.clear = CAVS_L2_IDC;
 
 	}
+
+	/* Clear out any existing pending interrupts that might be present */
+	for (int i = 0; i < CONFIG_MP_NUM_CPUS; i++) {
+		for (int j = 0; j < CONFIG_MP_NUM_CPUS; j++) {
+			IDC[i].core[j].tfc = BIT(31);
+		}
+	}
+
+	cpus_active[0] = true;
 }
 
 /**
