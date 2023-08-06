6aa5f9cc368bd1e70245bedb3cd3b6fc2f342d60
Andy Ross <andyross@google.com>
2023-08-05 20:02:25 -0700

    HACK: hello_world rig

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,31 @@
 
 #include <zephyr/kernel.h>
 
+static void timer_fn(struct k_timer *timer)
+{
+	static uint32_t last;
+	uint32_t now = k_cycle_get_32();
+	uint32_t dt = now - last;
+	printk("%s() dt %d\n", __func__, dt);
+	last = now;
+}
+
 int main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	printk("Busy waiting...\n");
+	k_busy_wait(300000);
+
+	printk("Entering a syscall...\n");
+	__asm__ volatile("syscall");
+	printk("came back, all done!\n");
+
+	struct k_timer t;
+	k_timer_init(&t, timer_fn, NULL);
+	printk("starting timer...\n");
+	k_timer_start(&t, K_MSEC(500), K_MSEC(1000));
+
+	while(1);
 	return 0;
 }
